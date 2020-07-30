31b4bc4dfee0bc9e70e969fe2ef6aaee0f416dfb
mvkitech <info@mvkitech.com>
2020-07-30 16:18:23 -0600

    Added changes in order to deploy to AWS

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin ${serverConfig.origin}`);
   }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
